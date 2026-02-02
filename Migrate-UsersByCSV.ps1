ecdfcd778117928e0967cb7b456e0dd3196c1c44
Iria Barciela Cabaleiro <139056916+IriaBarciela@users.noreply.github.com>
2026-02-02 09:25:52 +0100

    Add user migration script for Education group

---
+++ Migrate-UsersByCSV.ps1
@@ -0,0 +1,22 @@
+﻿$csv = Import-Csv -Delimiter ":" -Path Z:\Vigo-Ourense.csv -Encoding UTF8
+
+foreach ($usuario in $csv){
+
+    if ($usuario.Grupo -eq "Educación"){
+
+        $nombreusuario= Get-ADUser -Identity $usuario.LoginDefinitivo |select name
+        
+        $usuarioedu = ("CN=" + $nombreusuario.name + ",OU=Educación,OU=Consellerías,DC=xunta,DC=local")
+
+        Move-ADObject -Identity $usuarioedu `
+        -TargetPath ("OU=" + $usuario.Destino + ",OU=Educación,OU=Consellerías,DC=xunta,DC=local")
+
+        Write-Host ("Moviendo a " + $usuario.LoginDefinitivo +  " a " + $usuario.Destino)
+    
+    }else{
+
+        Write-Host "No es un usuario de educación"
+
+    }
+
+}
