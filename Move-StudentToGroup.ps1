816b429692f76c421bf87decc7f5a80523427140
Iria Barciela Cabaleiro <139056916+IriaBarciela@users.noreply.github.com>
2026-02-02 09:20:18 +0100

    Create Move-StudentToGroup.ps1 script for AD management
    This script moves a student to a specified group in Active Directory, handling user and group validation, and updating user attributes.

---
+++ Move-StudentToGroup.ps1
@@ -0,0 +1,57 @@
+﻿param (
+    [parameter(mandatory=$true)]$login,
+    [parameter(mandatory=$true)]$grupo
+)
+
+
+if ($login -eq "" -or $grupo -eq ""){
+        
+    Write-Host "El login o el grupo no pueden estar vacíos"
+    Write-Host "login: Debe ser el samacount del usuario"
+    Write-Host "grupo: Es el grupo al que queremos mover el usuario"
+
+}else {
+
+    if (Get-ADUser -Filter "samAccountName -like '$login'") {
+        
+        if (Get-ADGroup -Filter "SamAccountName -like 'Alumnos-$grupo'") {
+
+            $usuario = Get-ADUser $login
+
+
+            # Matricula
+
+            $OU = "OU=$grupo,OU=Alumnado,OU=Usuarios,OU=Sede-Vigo,DC=xilgaro-dm,DC=intranet"
+
+            Move-ADObject -Identity $usuario -TargetPath $OU -Confirm:$false
+
+
+            #Eliminar del anterior grupo
+
+            $OLD_Group = Get-ADPrincipalGroupMembership -Identity $login |select name |where name -like 'Alumnos*'
+
+            Remove-ADGroupMember $OLD_Group[0].name -Members $login -Confirm:$false
+
+
+            #Añadir al nuevo grupo
+
+            Add-ADGroupMember -identity "Alumnos-$grupo" -members $usuario 
+
+
+            # Poner descripcion
+
+            Set-aduser $login -description "Alumno de $grupo"
+
+        } else {
+
+        Write-Host "No se encontro el grupo $grupo"
+        
+        }
+
+
+    } else  {
+
+            Write-Host "No se encontro el usuario $login"
+
+      }
+}
