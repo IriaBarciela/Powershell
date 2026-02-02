ab135a306fe0ad6bad8397a82194421be9db96b9
Iria Barciela Cabaleiro <139056916+IriaBarciela@users.noreply.github.com>
2026-02-02 09:21:12 +0100

    Add script to create department structure in AD
    This script creates an Active Directory Organizational Unit and group for a specified department if they do not already exist.

---
+++ New-DepartmentStructure.ps1
@@ -0,0 +1,46 @@
+﻿param (
+    [parameter(mandatory=$true)]$departamento
+)
+
+if ($departamento -eq "") {
+
+    Write-Host "El departamento no puede estar vacío"
+
+} else {
+    $OU_generico = "OU=Profesorado,OU=Usuarios,OU=Sede-Vigo,DC=xilgaro-dm,DC=intranet"
+
+    $OU_completa = "OU=$departamento,$OU_generico"
+
+    # Cuidado esta linea hay que poner un distinguished name
+
+    $OU = "OU=$departamento,OU=Profesorado,OU=Usuarios,OU=Sede-Vigo,DC=xilgaro-dm,DC=intranet"
+
+    if (Get-ADOrganizationalUnit -Filter "distinguishedName -eq '$OU'") {
+
+        Write-Host "El departamento ya existe"
+
+    } else {
+
+        # Crear OU
+
+        New-ADOrganizationalUnit -Name $departamento -Path $OU_generico
+
+
+        Write-Host "Se creó la OU"
+    }
+
+    if (Get-ADGroup -Filter "Name -eq 'Profes_$departamento'") {
+
+        Write-Host "El grupo Profes_$departamento ya existe"
+
+    } else {
+
+        # Crear grupo
+        New-ADGroup -Name "Profes_$departamento" -GroupScope Global -Path $OU_completa -Description "Grupo de profesorado do departamento $departamento"
+        
+        Write-Host "Se creó el grupo"
+    }
+
+    Write-Host "Compeltado con exito"
+
+}
