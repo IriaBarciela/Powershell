c5d3d37dee0ccdd21f7e06cacd78e98fc9956512
Iria Barciela Cabaleiro <139056916+IriaBarciela@users.noreply.github.com>
2026-02-02 09:23:15 +0100

    Initialize script for student storage directories

---
+++ Initialize-StudentStorage.ps1
@@ -0,0 +1,39 @@
+﻿$carpetas = Get-ADOrganizationalUnit `
+    -SearchBase "OU=Alumnado,OU=Usuarios,OU=Sede-Vigo,DC=xilgaro-dm,DC=intranet" `
+    -Filter * -SearchScope OneLevel
+
+
+
+$carpetas.name | ForEach-Object {
+
+    $carpeta = $_
+
+    Write-Host "Creando carpeta: $carpeta"
+
+
+
+    New-Item -Path Z:\DatosXilgaro\Usuarios\Alumnado\$carpeta  -ItemType Directory
+
+    New-Item -Path Z:\DatosXilgaro\Recursos\Alumnado\$carpeta  -ItemType Directory
+
+
+
+}
+
+foreach ($carpeta in $carpetas.name ) {
+
+
+    Write-Host "Creando asignaturas para: $carpeta"
+
+
+    $modulos = Get-Content -Path "Z:\Asignaturas\$carpeta.txt" -Encoding Ascii
+
+
+    foreach ($modulo in $modulos) {
+        
+
+        New-Item -Path "Z:\DatosXilgaro\Usuarios\Alumnado\$carpeta\$modulo" -ItemType Directory
+        New-Item -Path "Z:\DatosXilgaro\Recursos\Alumnado\$carpeta\$modulo" -ItemType Directory
+    
+    }
+}
